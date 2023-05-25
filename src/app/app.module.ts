77e7d11ccf6844b6e6d4ecc75ba47e616b0aeeac
pcsidque <pcsidque@upc.edu.pe>
2023-05-25 16:30:57 -0500

    feat: created base structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TransferComponent } from './funds/pages/transfer/transfer.component';
+import { HomeComponent } from './public/pages/home/home.component';
+import { HeaderComponent } from './public/components/header/header.component';
+import { FooterComponent } from './public/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransferComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
