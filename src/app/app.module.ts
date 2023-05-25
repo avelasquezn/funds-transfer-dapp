c7da975dab84251f364a7100cad33b3e706f2306
pcsidque <pcsidque@upc.edu.pe>
2023-05-25 18:53:04 -0500

    feat: added public and funds ui elements.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TransferComponent } from './funds/pages/transfer/transfer.component';
-import { HomeComponent } from './public/pages/home/home.component';
-import { HeaderComponent } from './public/components/header/header.component';
-import { FooterComponent } from './public/components/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TransferComponent} from './funds/pages/transfer/transfer.component';
+import {HomeComponent} from './public/pages/home/home.component';
+import {HeaderComponent} from './public/components/header/header.component';
+import {FooterComponent} from './public/components/footer/footer.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSelectModule} from "@angular/material/select";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -20,9 +27,18 @@ import { FooterComponent } from './public/components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/funds/pages/transfer/transfer.component.ts
+++ src/app/funds/pages/transfer/transfer.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.component.html',
   styleUrls: ['./transfer.component.css']
 })
-export class TransferComponent {
+export class TransferComponent implements OnInit {
+  formSubmitted = false;
+  userForm: FormGroup;
+  user: any;
 
+  accountValidationMessages = {
+    transferAddress: [
+      { type: 'required', message: 'Transfer Address is required'},
+      { type: 'minLength', message: 'Transfer Address must be 42 characters long'},
+      { type: 'maxlength', message: 'Transfer Address must be 42 characters long'}
+    ],
+    amount: [
+      { type: 'required', message: 'Amount is required'},
+      { type: 'pattern', message: 'AMount must be a positive number'}
+    ],
+    remarks: [
+      {type: 'required', message: 'Remarks are required'}
+    ]
+  }
+  constructor(private builder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formSubmitted = false;
+    this.user = { address: '', transferAddress: '', balance: '', amount: '', remarks: '' };
+    this.getAccountBalance();
+    this.createForms();
+  }
+
+  createForms() {
+    this.userForm = this.builder.group({
+      transferAddress: new FormControl(this.user.transferAddress, Validators.compose([
+        Validators.required,
+        Validators.minLength(42),
+        Validators.maxLength(42)
+      ])),
+      amount: new FormControl(this.user.amount, Validators.compose([
+        Validators.required,
+        Validators.pattern('ˆ[+]?([.]\\d+|\\d+[.]?\\d*)$')
+      ])),
+      remarks: new FormControl(this.user.remarks, Validators.compose([
+        Validators.required
+      ]))
+    });
+  }
+
+  getAccountBalance = () => {
+    const that = this;
+    that.user.address = '';
+    that.user.balance = 0;
+    // TODO: fetch data
+  }
+
+  submitForm() {
+    if (this.userForm.invalid) {
+      alert('transfer.component :: submitForm :: Form invalid');
+      return;
+    } else {
+      console.log('transfer.component :: submitForm :: this.userForm.value');
+      console.log(this.userForm.value);
+      // TODO: service call
+    }
+  }
 }
